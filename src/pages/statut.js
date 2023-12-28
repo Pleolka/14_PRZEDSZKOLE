ee178ab9b8ace8d2fdc40ba99f51cceb9da4399a
Aleksandra Fabian <pleolka@gmail.com>
2023-12-28 18:06:33 +0100

    statut do pobrania

--- src/pages/statut.js
+++ src/pages/statut.js
@@ -1,16 +1,29 @@
 import React from "react"
-import { graphql, useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery } from "gatsby"
 
-import Layout from '../components/layout'
-import SEO from '../components/SEO'
+import Layout from "../components/layout"
+import SEO from "../components/SEO"
 
 const Statut = () => {
-
   const data = useStaticQuery(graphql`
     query {
+      icon: file(relativePath: { eq: "icon.png" }) {
+        childImageSharp {
+          fixed(width: 30) {
+            ...GatsbyImageSharpFixed_noBase64
+          }
+        }
+      }
+
       allContentfulInformacjeDodatkoweOPrzedszkolu {
         edges {
           node {
+            statutDoPobrania {
+              description
+              file {
+                url
+                fileName
+              }
             statut {
               childMarkdownRemark {
                 html
@@ -19,18 +32,38 @@ const Statut = () => {
           }
         }
       }
-    }`)
+    }
+  `)
 
-  const statutHTML = data.allContentfulInformacjeDodatkoweOPrzedszkolu.edges[0].node.statut.childMarkdownRemark.html
+  const statutHTML =
+    data.allContentfulInformacjeDodatkoweOPrzedszkolu.edges[0].node.statut
+      .childMarkdownRemark.html
 
   return (
     <Layout>
       <SEO title="Statut przedszkola" />
 
-      <div className="container statut"
-        dangerouslySetInnerHTML={{ __html: statutHTML }}>
-      </div>
+      <div
+        className="container statut"
+        dangerouslySetInnerHTML={{ __html: statutHTML }}
+      ></div>
 
+      <h3>Do pobrania</h3>
+
+      <a
+        href={
+          data.allContentfulInformacjeDodatkoweOPrzedszkolu.edges[0].node
+            .statutDoPobrania.file.url
+        }
+      >
+        <Img fixed={icon} alt="logo" />
+        <p>
+          {
+            data.allContentfulInformacjeDodatkoweOPrzedszkolu.edges[0].node
+              .statutDoPobrania.file.fileName
+          }
+        </p>
+      </a>
     </Layout>
   )
 }
