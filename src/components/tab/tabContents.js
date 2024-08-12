4e1ce93e50a8815e803c4944405b436345be295f
Aleksandra Fabian <pleolka@gmail.com>
2024-08-12 13:02:55 +0200

    Praca nad standardami

--- src/components/tab/tabContents.js
+++ src/components/tab/tabContents.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-const TabContents = (props) => {
-    return (
-        <div
-            className={props.className}
-            style={props.style}
-            data-tab={props.dataTab}>
-            {documentToReactComponents(props.content)}
-        </div>
-    );
+const TabContents = props => {
+  return (
+    <div
+      className={props.className}
+      style={props.style}
+      data-tab={props.dataTab}
+    >
+      {documentToReactComponents(props.content)}
+    </div>
+  )
 }
 
 export default TabContents
--- src/pages/dlarodzicow.js
+++ src/pages/dlarodzicow.js
@@ -1,39 +1,38 @@
 import React from "react"
-import { graphql, useStaticQuery } from 'gatsby'
-import Img from 'gatsby-image'
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
-import Layout from '../components/layout'
-import Tab from '../components/tab/tab'
-import SEO from '../components/SEO'
+import Layout from "../components/layout"
+import Tab from "../components/tab/tab"
+import SEO from "../components/SEO"
 
 const DlaRodzicow = () => {
-
   const data = useStaticQuery(graphql`
     query {
-        kids: file(relativePath: { eq: "kids01.png" }) {
-            childImageSharp {
-              fluid(quality: 100, maxWidth: 900) {
-                ...GatsbyImageSharpFluid_noBase64
-              }
-            }
+      kids: file(relativePath: { eq: "kids01.png" }) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 900) {
+            ...GatsbyImageSharpFluid_noBase64
           }
-
-          allContentfulDlaRodzicow(sort: {fields: idNumber, order: ASC}) {
-            nodes {
-              data
-              tytul
-              idNumber
-              childContentfulDlaRodzicowOpisRichTextNode {
-                json
-                childContentfulRichText {
-                  html
-                }
-              }
-            }
-          }
+        }
       }
-      
-    `)
+
+      allContentfulDlaRodzicow(sort: { fields: idNumber, order: ASC }) {
+        nodes {
+          data
+          tytul
+          idNumber
+          doPobrania
+          childContentfulDlaRodzicowOpisRichTextNode {
+            json
+            childContentfulRichText {
+              html
+            }
+          }
+        }
+      }
+    }
+  `)
 
   const kids = data.kids.childImageSharp.fluid
   const content = data.allContentfulDlaRodzicow.nodes
@@ -43,29 +42,35 @@ const DlaRodzicow = () => {
       <SEO title="Dla rodziców" />
 
       <div className="container dlarodzicow">
-
         <h1 className="first-section">Dla Rodziców</h1>
 
         <Tab data={content} />
 
-        {content.map((node) => {
-
+        {content.map(node => {
           return (
-
             <div className="mobile-only">
               <h5>{node.tytul}</h5>
-              <div className={node.tytul === "Ramowy rozkład dnia" ? "flex2-2 flexP" : ""}
-                dangerouslySetInnerHTML={{ __html: node.childContentfulDlaRodzicowOpisRichTextNode.childContentfulRichText.html }}></div>
+              <div
+                className={
+                  node.tytul === "Ramowy rozkład dnia" ? "flex2-2 flexP" : ""
+                }
+                dangerouslySetInnerHTML={{
+                  __html:
+                    node.childContentfulDlaRodzicowOpisRichTextNode
+                      .childContentfulRichText.html,
+                }}
+              ></div>
             </div>
           )
-        })
-        }
+        })}
       </div>
 
-      <Img fluid={kids}
+      <Img
+        fluid={kids}
         className="footer-image"
         objectFit="cover"
-        alt="kids playing the music" />
+        alt="kids playing the music"
+      />
     </Layout>
   )
 }
