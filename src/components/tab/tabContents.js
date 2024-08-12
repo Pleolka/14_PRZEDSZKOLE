3905073f960f26dec54db92a1f55ef065e376dae
Aleksandra Fabian <pleolka@gmail.com>
2024-08-12 13:18:13 +0200

    Poprawka w importch

--- src/components/tab/tabContents.js
+++ src/components/tab/tabContents.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Img from "gatsby-image"
 
