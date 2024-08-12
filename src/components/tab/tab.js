188f17d2b949cf6e5a795629ecbf6d422d44b026
Aleksandra Fabian <pleolka@gmail.com>
2024-08-12 12:59:55 +0200

    Tab

--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -1,56 +1,52 @@
-import React from 'react'
-import TabMenu from './tabMenu'
-import TabContents from './tabContents'
+import React from "react"
+import TabMenu from "./tabMenu"
+import TabContents from "./tabContents"
 
 class Tab extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { isActive: "1" };
-        this.changeActive = this.changeActive.bind(this);
-    }
+  constructor(props) {
+    super(props)
+    this.state = { isActive: "1" }
+    this.changeActive = this.changeActive.bind(this)
+  }
 
-    changeActive(ev) {
-        this.setState({ isActive: ev.target.getAttribute("data-tab") });
-    }
+  changeActive(ev) {
+    this.setState({ isActive: ev.target.getAttribute("data-tab") })
+  }
 
-    render() {
+  render() {
+    const listTabMenu = this.props.data.map(node => (
+      <TabMenu
+        isActive={this.state.isActive}
+        onClick={this.changeActive}
+        dataTab={node.idNumber.toString()}
+        title={node.tytul}
+      />
+    ))
 
-        const listTabMenu = this.props.data.map((node) =>
-            < TabMenu
-                isActive={this.state.isActive}
-                onClick={this.changeActive}
-                dataTab={node.idNumber.toString()}
-                title={node.tytul}
-            />
-
-        );
-
-        const listContent = this.props.data.map((node) =>
-            this.state.isActive === node.idNumber.toString() ? (
-                <TabContents
-                    className={node.tytul === "Ramowy rozkład dnia" ? "flexP" : ""}
-                    dataTab={node.idNumber.toString()}
-                    content={node.childContentfulDlaRodzicowOpisRichTextNode.json} />
-            ) : (
-                    <TabContents
-                        style={{ display: "none" }}
-                        dataTab={node.idNumber.toString()}
-                        content={node.childContentfulDlaRodzicowOpisRichTextNode.json}
-                    />
-                )
-        );
-        return (
-
-            <div className="tab" >
-                <ul className="tab-menu">
-                    {listTabMenu}
-                </ul>
-                <div className="tab-content">
-                    {listContent}
-                </div>
-            </div>
-        );
-    }
+    const listContent = this.props.data.map(node =>
+      this.state.isActive === node.idNumber.toString() ? (
+        <TabContents
+          className={node.tytul === "Ramowy rozkład dnia" ? "flexP" : ""}
+          dataTab={node.idNumber.toString()}
+          content={node.childContentfulDlaRodzicowOpisRichTextNode.json}
+          doPobrania={node.doPobrania ? node.doPobrania : ""}
+        />
+      ) : (
+        <TabContents
+          style={{ display: "none" }}
+          dataTab={node.idNumber.toString()}
+          content={node.childContentfulDlaRodzicowOpisRichTextNode.json}
+          doPobrania={node.doPobrania ? node.doPobrania : ""}
+        />
+      )
+    )
+    return (
+      <div className="tab">
+        <ul className="tab-menu">{listTabMenu}</ul>
+        <div className="tab-content">{listContent}</div>
+      </div>
+    )
+  }
 }
 
 export default Tab
