5be29014e0722367a27cffa88ade9256ae2a8dad
Aleksandra Fabian <pleolka@gmail.com>
2024-08-12 13:15:17 +0200

    Standardy ochrony małoletnich

--- src/components/tab/tabContents.js
+++ src/components/tab/tabContents.js
@@ -1,7 +1,20 @@
 import React from "react"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Img from "gatsby-image"
 
 const TabContents = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      icon: file(relativePath: { eq: "icon.png" }) {
+        childImageSharp {
+          fixed(width: 30) {
+            ...GatsbyImageSharpFixed_noBase64
+          }
+        }
+      }
+    }
+  `)
+  const icon = data.icon.childImageSharp.fixed
   return (
     <div
       className={props.className}
@@ -9,7 +22,17 @@ const TabContents = props => {
       data-tab={props.dataTab}
     >
       {documentToReactComponents(props.content)}
-      {props.doPobrania ? <p>{props.doPobrania.file.fileName}</p> : ""}
+      {props.doPobrania ? (
+        <>
+          <h3>Do pobrania</h3>
+          <a href={props.doPobrania.file.url}>
+            <Img fixed={icon} alt="logo" />
+            <p>{props.doPobrania.file.fileName}</p>
+          </a>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   )
 }
